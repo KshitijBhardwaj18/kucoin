c0cb168a3d3d709d9f01a72b9a7a9a7e7ccb0a2b
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2024-03-06 00:23:49 +0530

    Completed navbar desktop version

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,26 +1,59 @@
 import Image from "next/image";
 
+interface navbarProps {}
 
-interface navbarProps {
-
-}
+const navItems = [
+  {
+    label: "Crypto Taxes",
+    link: "/",
+  },
+  {
+    label: "Free Tools",
+    link: "/",
+  },
+  {
+    label: "Resource Center",
+    link: "/",
+  },
+];
 
 const Navbar: React.FC<navbarProps> = () => {
-    return ( 
-        <div className="h-[64px] bg-white flex flex-col justify-center border-b">
-            <div className="h-full mx-[24px] flex justify-between">
-                <div className=" flex flex-col justify-center">
-                <Image alt="logo" src="/images/logo.png" width={81} height={60.75}  /> 
-               
-                </div>
-
-                <div className=" flex flex-col justify-center">
-                <Image alt="logo" src="/images/menu.png" width={20.56} height={13.71}  /> 
-               
-                </div>
-            </div>
+  return (
+    <div className="h-[64px] bg-white flex flex-col justify-center border-b drop-shadow">
+      <div className="h-full mx-[24px] flex justify-between">
+        <div className=" flex flex-col justify-center">
+          <Image alt="logo" src="/images/logo.png" width={81} height={60.75} />
         </div>
-     );
-}
- 
+
+        <div className="flex flex-col justify-center sm:hidden">
+          <Image
+            alt="logo"
+            src="/images/menu.png"
+            width={20.56}
+            height={13.71}
+          />
+        </div>
+
+        <div className="hidden sm:flex sm:flex-row justify-evenly">
+          <div className="flex  justify-center gap-3 ">
+            {navItems.map((item) => (
+              <div
+                key={item.label}
+                className="font-semibold text-[14px] flex flex-col justify-center text-[#0F1629] leading-[19.36px] "
+              >
+                {item.label}
+              </div>
+            ))}
+            <div className="flex flex-col justify-center">
+              <div className="h-[40px] w-[136px] rounded p-2 bg-gradient-to-r from-blue-500 via-blue-700 to-blue-900 text-center text-white font-[600] text-[16px] leading-[19.36px] flex flex-col justify-center ">
+                Get Started
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Navbar;
